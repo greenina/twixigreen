252679e4f3ec9baf2d9d00eeec6cceadb0054f25
Kijoon <igreen0485@gmail.com>
2021-06-07 01:30:14 +0900

    aaa

--- src/components/MainPage/index.js
+++ src/components/MainPage/index.js
@@ -2,14 +2,10 @@ import './style.css';
 import { db, firebaseApp } from '../../firebase';
 import React, { useEffect, useState } from 'react';
 import Tippy from 'react-tooltip';
-<<<<<<< HEAD
-=======
-import { useSelector, useDispatch } from 'react-redux';
-//import Arrow from './Arrow'
+
 
 import leftArrow from '../../img/left-arrow.svg';
 import rightArrow from '../../img/right-arrow.svg';
->>>>>>> c3ae53f9c3426728590d753ec394c1316cb14f61
 //import Switch from './Switch';
 
 function MainPage() {
